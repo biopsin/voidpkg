8863bde0ecd630aed7fa92942eb58a265ce28897
biopsin <60029989+biopsin@users.noreply.github.com>
2020-03-18 23:08:16 +0100

    xsetnumlock: code fork
    kudos to you buddy whom ever you are :)

---
+++ xsetnumlock/files/xsetnumlock.c
@@ -0,0 +1,17 @@
+#include <X11/extensions/XTest.h>
+#include <X11/keysym.h>
+
+int main(void)
+{
+Display* disp = XOpenDisplay(NULL);
+
+if (disp == NULL) return 1;
+
+XTestFakeKeyEvent(disp, XKeysymToKeycode(disp, XK_Num_Lock),
+	True, CurrentTime);
+XTestFakeKeyEvent(disp, XKeysymToKeycode(disp, XK_Num_Lock),
+	False, CurrentTime );
+XCloseDisplay(disp);
+
+return 0;
+}
